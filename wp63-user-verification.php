d6bbfb394a00daa7cd26087277fca2d146be1535
Jirayu Yingthawornsuk <me@jirayu.in.th>
2017-12-14 17:52:29 +0700

    initial commit

---
+++ wp63-user-verification.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: User Verification
+Plugin URI: https://github.com/IronGhost63/wp63-user-verification
+Author: Jirayu Yingthawornsuk
+Author URI: https://jirayu.in.th
+Description: A WordPress Plugin forcing user to verify account with verification code from email
+Version: 1.0
+Text Domain: wp63uv
+*/
+?>
