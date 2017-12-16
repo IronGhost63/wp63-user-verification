aa510ee706687eb5b1c5bc661d306e5ad5550414
Jirayu Yingthawornsuk <me@jirayu.in.th>
2017-12-16 13:54:10 +0700

    reformat code

--- wp63-user-verification.php
+++ wp63-user-verification.php
@@ -144,13 +144,14 @@ function myplugin_woocommerce_locate_template( $template, $template_name, $templ
 
 	$_template = $template;
 
-	if ( ! $template_path ) $template_path = $woocommerce->template_url;
+	if ( ! $template_path ) {
+		$template_path = $woocommerce->template_url;
+	}
 
 	$plugin_path  = WP63UV_PATH . '/woocommerce/';
 
 	// Look within passed path within the theme - this is priority
 	$template = locate_template(
-
 		array(
 			$template_path . $template_name,
 			$template_name
@@ -158,13 +159,15 @@ function myplugin_woocommerce_locate_template( $template, $template_name, $templ
 	);
 
 	// Modification: Get the template from this plugin, if it exists
-	if ( ! $template && file_exists( $plugin_path . $template_name ) )
+	if ( ! $template && file_exists( $plugin_path . $template_name ) ){
 		$template = $plugin_path . $template_name;
+	}
 
 	// Use default template
-	if ( ! $template )
+	if ( ! $template ) {
 		$template = $_template;
-
+	}
+	
 	// Return what we found
 	return $template;
 }
